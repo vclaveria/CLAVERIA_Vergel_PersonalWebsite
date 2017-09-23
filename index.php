eda6b4309c4d4783df1fd1b3af5c9f99a81ed6e7
vclaveria <claveriavergel@gmail.com>
2017-09-24 01:44:16 +0800

    naa na sya temporary background and menu bar

--- index.php
+++ index.php
@@ -1,9 +1,76 @@
+
+<style> 
+nav {
+text-align:center;
+font-size: 1em;
+	}
+
+nav li {
+display:inline-block;
+padding:0 40px;
+	}
+
+nav a {
+color: black;	
+text-decoration:none; 	
+	}
+
+nav a:hover {
+  color: #000;
+	}
+
+nav a::before {
+  position: relative;
+  content: '';
+  height: 5px;
+  background-color: #444;
+  display: block;
+  top: 0;
+  width: 0%;
+  transition: all ease-in-out 250ms;
+	}
+
+nav a:hover::before {
+  width: 100%;
+	}
+
+body {
+    margin: 470px 0 100px 0;
+    background: white;
+    font-family: Georgia, "Times New Roman", serif;
+    font-size: 14px;
+    line-height: 1.5;
+	background-image: url("1234	.jpg");
+    background-repeat: no-repeat;
+	}
+
+</style>
+
 <html>
-<body>
-	<h3>Vergel Gomez Claveria Profiles<h3><br>
+<title>
+Vergel Profile
+</title>
+<body> 
 	
 	
+	
+<center><h1>VERGEL GOMEZ CLAVERIA</h1></center>
 
 
+	<div class="navbar">
+	<nav class="navbar">
+	<ul id="nav">
+		<li><a href="#">HOME</a></li>
+		<li><a href="#">PROFILE</a></li>
+		<li><a href="#">INTEREST</a></li>
+		<li><a href="#">ACHIEVEMENT</a></li>
+		<li><a href="#">ASPIRATION</a></li>
+    </ul>
+	</nav>
+	</div>
+		
+	
+		
+</body>
 </body>
 </html>
