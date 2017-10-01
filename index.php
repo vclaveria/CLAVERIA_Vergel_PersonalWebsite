7bcdbc1a41efe9ff7c7b1144cd4e7999da69f6da
vclaveria <claveriavergel@gmail.com>
2017-10-01 11:49:26 +0800

    designing the button with css

--- index.php
+++ index.php
@@ -4,10 +4,26 @@ body {
   
     font-family: Georgia, "Arial", serif;
   	background-image: url("./images/funnydog.jpg");
-	  background-size: cover;	
-    
+	  background-size: cover;	 
 	}
 
+.btn-group .button {
+    margin: 470px 0 5px 12;
+    background-color: transparent;
+    border: 4px solid white;
+    color: white;
+    padding: 15px 25px;
+    display: inline-block;
+    font-size: 18px;
+    cursor: pointer;
+    border-radius: 12px;
+    
+  }
+
+  .btn-group .button:hover {
+    background-color: white;
+    color: black;
+  }  
 
 	
 	
@@ -16,20 +32,20 @@ body {
 
 
 <html>  
-<title>Vergel Profile</title>
+  <title>Vergel Profile</title>
 
-<body> <center><h2>WELCOME TO VERGEL WEBSITE</h2> </center>
+  <body> <center><h2>WELCOME TO VERGEL WEBSITE</h2> </center>
 
-<div class="btn-group">	<center>
+    <div class="btn-group">	<center>
 
-    <button>ABOUT ME</button> 
-    <button>EDUCATION</button>
-    <button>ACHIEVEMENT</button>
-    <button>ASPIRATION</button>	
-    <button>INTEREST</button>	</center>
-    
-</div>
+      <button class="button">ABOUT ME</button> 
+      <button class="button">EDUCATION</button>
+      <button class="button">ACHIEVEMENT</button>
+      <button class="button">ASPIRATION</button>	
+      <button class="button">INTEREST</button>	</center>
+
+    </div>
 
 
-</body>
+  </body>
 </html>
