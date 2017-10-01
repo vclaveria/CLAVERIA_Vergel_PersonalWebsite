fe50be3b27813c7986c665a40c0ebb1853b3479d
vclaveria <claveriavergel@gmail.com>
2017-10-01 11:41:03 +0800

    adding button

--- index.php
+++ index.php
@@ -1,76 +1,35 @@
-
 <style> 
-nav {
-text-align:center;
-font-size: 1em;
-	}
-
-nav li {
-display:inline-block;
-padding:0 40px;
-	}
-
-nav a {
-color: black;	
-text-decoration:none; 	
-	}
-
-nav a:hover {
-  color: #000;
-	}
-
-nav a::before {
-  position: relative;
-  content: '';
-  height: 5px;
-  background-color: #444;
-  display: block;
-  top: 0;
-  width: 0%;
-  transition: all ease-in-out 250ms;
-	}
-
-nav a:hover::before {
-  width: 100%;
-	}
 
 body {
-    margin: 470px 0 100px 0;
-    background: white;
-    font-family: Georgia, "Times New Roman", serif;
-    font-size: 14px;
-    line-height: 1.5;
-	background-image: url("1234	.jpg");
-    background-repeat: no-repeat;
+  
+    font-family: Georgia, "Arial", serif;
+  	background-image: url("./images/funnydog.jpg");
+	  background-size: cover;	
+    
 	}
 
+
+	
+	
 </style>
 
-<html>
-<title>
-Vergel Profile
-</title>
-<body> 
-	
-	
-	
-<center><h1>VERGEL GOMEZ CLAVERIA</h1></center>
 
 
-	<div class="navbar">
-	<nav class="navbar">
-	<ul id="nav">
-		<li><a href="#">HOME</a></li>
-		<li><a href="#">PROFILE</a></li>
-		<li><a href="#">INTEREST</a></li>
-		<li><a href="#">ACHIEVEMENT</a></li>
-		<li><a href="#">ASPIRATION</a></li>
-    </ul>
-	</nav>
-	</div>
-		
-	
-		
-</body>
+<html>  
+<title>Vergel Profile</title>
+
+<body> <center><h2>WELCOME TO VERGEL WEBSITE</h2> </center>
+
+<div class="btn-group">	<center>
+
+    <button>ABOUT ME</button> 
+    <button>EDUCATION</button>
+    <button>ACHIEVEMENT</button>
+    <button>ASPIRATION</button>	
+    <button>INTEREST</button>	</center>
+    
+</div>
+
+
 </body>
 </html>
